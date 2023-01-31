5cc84edb0ad49c0c0b076898817a03f77535f0b2
MikeyCouture <michelcouture42@gmail.com>
2023-01-31 16:00:05 -0500

    logo added and styling for nav added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./routes/home/home.component";
 import Nav from "./routes/Navigation/nav.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return (
@@ -16,6 +18,7 @@ const App = () => {
       <Route path="/" element={<Nav />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/Navigation/nav.component.jsx
+++ src/routes/Navigation/nav.component.jsx
@@ -1,16 +1,22 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import "./nav.styles.scss";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 const Nav = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>logo</div>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             Shop
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
           </Link>
         </div>
       </div>
